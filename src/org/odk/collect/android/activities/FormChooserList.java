54cdf63ceb5fbe0c705211390366d71fe7ac3d7c
Steven Nsubuga <snsubuga@applab.org>
2012-07-24 11:19:46 +0300

    removed autogenerated comments

--- src/org/odk/collect/android/activities/FormChooserList.java
+++ src/org/odk/collect/android/activities/FormChooserList.java
@@ -244,10 +244,8 @@ public class FormChooserList extends ListActivity implements DiskSyncListener {
 	        inputStream.close();
 	        outputStream.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
